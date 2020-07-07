8617ba85b562192a4eea32877eee73f21507ff34
Matthias Hölzl <tc@xantira.com>
2020-07-07 07:34:01 +0200

    Add Shape example

--- CleanCode/clean_code.cpp
+++ CleanCode/clean_code.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
+#include "complex_function.h"
 
-int main() {
-    std::cout << "Clean code examples" << std::endl;
+int main()
+{
+    std::string formatted_text { format_text(
+        "This is the text to format\n",
+        "<<< A header >>>\n",
+        "<<< A footer >>>\n",
+        10) };
+    std::cout << formatted_text << std::endl;
+
+    FormatText format_text_object(
+        "This is the text to format with the object\n",
+        "<<< A header >>>\n",
+        "<<< A footer >>>\n",
+        10);
+
+    formatted_text = format_text_object.compute(); 
+    
+    std::cout << formatted_text << std::endl;
+
 }
--- CleanCode/names.cpp
+++ CleanCode/names.cpp
@@ -1,12 +1,14 @@
+#include <iostream>
+#include <string>
 #include <vector>
 
 class BadNames1 {
-    std::vector<std::pair<int, int>> game_board;
+    std::vector<std::pair<int, int>> theList;
 
-    std::vector<std::pair<int,int>> getThem()
+    std::vector<std::pair<int, int>> getThem()
     {
         std::vector<std::pair<int, int>> list1 {};
-        for (std::pair<int, int> x : game_board) {
+        for (std::pair<int, int> x : theList) {
             if (x.first == 4) {
                 list1.push_back(x);
             }
@@ -14,10 +16,9 @@ class BadNames1 {
     }
 };
 
-
-
-class BadNames2 {
-    enum class Status { Flagged, Unflagged};
+class MineSweeper {
+    enum class Status { Flagged,
+        Unflagged };
     struct Cell {
         Status status { Status::Flagged };
         int bomb_count { 0 };
@@ -28,11 +29,12 @@ class BadNames2 {
     std::vector<Cell> getFlaggedCells()
     {
         std::vector<Cell> flagged_cells {};
-        for (Cell x : game_board) {
-            if (x.status == Status::Flagged) {
-                flagged_cells.push_back(x);
+        for (Cell cell : game_board) {
+            if (cell.status == Status::Flagged) {
+                flagged_cells.push_back(cell);
             }
         }
+        return flagged_cells;
     }
 };
 
@@ -57,6 +59,8 @@ const int INCLUDE_BOTH_DATES = 3;
 int MY_CONTROLLER_FOR_EFFICIENT_HANDLING_OF_STRINGS = 1;
 int MY_CONTROLLER_FOR_EFFICIENT_STORING_OF_STRINGS = 2;
 
+// Consistent
+
 class ObjectManager;
 class ObjectController;
 
@@ -74,10 +78,163 @@ class Triple {
     int third;
 };
 
+class CalenderDate;
+class SerializableDate;
 
-void copy(char a1[], char a2[]) {
+class SerialDate {
+    virtual int toSerial() = 0;
+    // ...
+};
+
+class UnixDate : public SerialDate {
+    int unix_timestamp { 0 };
+    int toSerial() override
+    {
+        return unix_timestamp; // Unix timestamp
+    }
+};
+
+void copy(char a1[], char a2[])
+{
     while (*(a2++) = *(a1++))
         ;
 }
 
+void copy_2(char from[], char to[])
+{
+    while (*(to++) = *(from++))
+        ;
+}
+
 int* pi_target;
+int* target;
+
+class IActor {
+};
+class Actor : public IActor {
+};
+
+class Actor2 {
+};
+class ActorImpl : public Actor2 {
+};
+
+class File {
+    bool is_open() const { return true; }
+};
+
+// Wrong part of speech
+class GoToTheServer {
+    void connection() { }
+    bool server_availability() { return false; }
+};
+
+class ServerConnection {
+    void connect() { }
+    bool is_server_available() { return false; }
+};
+
+// Functions
+
+int my_fun(int a, int b)
+{
+    int c { 0 };
+    int d { 0 };
+    // Do something complex
+    // Do something else
+    return 0;
+}
+
+class MyFun {
+    int a;
+    int b;
+    int c { 0 };
+    int d { 0 };
+
+public:
+    MyFun(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    void do_something_complex() { }
+    void do_something_else() { }
+
+    int compute()
+    {
+        do_something_complex();
+        do_something_else();
+        return 0;
+    }
+};
+
+void f()
+{
+    int i { my_fun(1, 2) };
+
+    MyFun my_new_fun { 1, 2 };
+    int j { my_new_fun.compute() };
+}
+
+class Employee {
+    virtual int calculate_pay(const std::string& name) = 0;
+};
+
+class CommissionedEmployee : public Employee {
+    int calculate_pay(const std::string& name) override
+    {
+        return 4500;
+    }
+};
+
+class HourlyEmployee : public Employee {
+    int calculate_pay(const std::string& name) override
+    {
+        return 1500;
+    }
+};
+
+Employee* makeEmployee(int id)
+{
+    if (id < 100) {
+        return new CommissionedEmployee();
+    } else {
+        return new HourlyEmployee();
+    }
+}
+
+int my_long_arglist_function(int a, int b, int c);
+int my_long_arglist_function_2(float x, int a, int b, int c);
+
+struct GridPosition {
+    int a;
+    int b;
+    int c;
+};
+
+int my_long_arglist_function(GridPosition g);
+int my_long_arglist_function_2(float x, GridPosition g);
+
+int print_text(std::string text, bool should_print_header = false)
+{
+    if (should_print_header) {
+        std::cout << "Header" << std::endl;
+    }
+    std::cout << text << std::endl;
+    return 0;
+}
+
+int print_text_without_header(std::string text)
+{
+    std::cout << text << std::endl;
+    return 0;
+}
+
+int print_text_with_header(std::string text)
+{
+    std::cout << "Header" << std::endl;
+    std::cout << text << std::endl;
+
+    return 0;
+}
